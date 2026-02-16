9878f5677c156399e7ea783bebbb67b265e2544f
rifatsm <rifatsm@vt.edu>
2026-02-16 15:00:17 -0500

    add attributes to manufacturer class.
    Added attr:
     - name
     - country
    
    Also add getter methods via @property.

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ from manufacturer import Manufacturer
 
 def main():
     # print("Hello World")
-    Manufacturer.print_me()
+    # Manufacturer.print_me()
+    m = Manufacturer("Ford", "USA")
+    # m.print_me()
+    # print(m._name) # DO NOT access private variable
+    print(m.get_name)
+    print(m.get_country)
     pass
 
 if __name__ == "__main__":
--- manufacturer.py
+++ manufacturer.py
@@ -3,6 +3,20 @@ This represents a vehicle manufacturer.
 '''
 
 class Manufacturer:
+    # constructor
+    def __init__(self, name: str, country: str):
+        self._name = name
+        self._country = country
+        
 
-    def print_me():
+    # getters 
+    @property
+    def get_name(self) -> str:
+        return self._name
+
+    @property
+    def get_country(self) -> str:
+        return self._country
+
+    def print_me(self):
         print("Hello Manufacturer")
