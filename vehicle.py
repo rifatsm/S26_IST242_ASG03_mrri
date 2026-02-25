aade708977888988cdedb65b6a44e193d282a8a4
rifatsm <rifatsm@vt.edu>
2026-02-25 15:23:48 -0500

    code: add concrete methods.

--- vehicle.py
+++ vehicle.py
@@ -31,3 +31,7 @@ class Vehicle(ABC):
     def mpg(self) -> float:
         return self._mpg
 
+    # ------ concrete methods -----
+    def how_far_with(self,
+                     num_of_gallons: int) -> float:
+        return self._mpg * num_of_gallons
